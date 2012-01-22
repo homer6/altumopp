cf8787e776db471c300c1aec14b44e5a56fefc01
Steve Sperandeo <steve.sperandeo@gmail.com>
2012-01-21 22:11:30 -0800

    Added valgrind profiler call to test.php.

--- src/extensions/php/test.php
+++ src/extensions/php/test.php
@@ -1,6 +1,10 @@
 <?php
 	// ./configure --with-php-config=/path/to/correct/php-config
 
+	//  valgrind --tool=callgrind --dump-instr=yes --trace-jump=yes php test.php
+
+	sleep( 10 );
+
 	echo altumo_run();
 
 
